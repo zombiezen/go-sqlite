e71923e1fc6c5cb5ca85406cef177d23f659c429
Ross Light <ross@zombiezen.com>
2022-01-09 21:09:28 -0800

    Set limited timeout for WAL

--- sqlite.go
+++ sqlite.go
@@ -112,22 +112,26 @@ func OpenConn(path string, flags ...OpenFlags) (*Conn, error) {
 		return nil, fmt.Errorf("sqlite: open %q: disable double-quoted string literals: %v", path, err)
 	}
 
-	c.SetBlockOnBusy()
-
 	if openFlags&OpenWAL != 0 {
+		// Set timeout for enabling WAL.
+		// See https://github.com/crawshaw/sqlite/pull/113 for details.
+		// TODO(maybe): Pass in Context to OpenConn?
+		c.SetBusyTimeout(10 * time.Second)
+
 		stmt, _, err := c.PrepareTransient("PRAGMA journal_mode=wal;")
 		if err != nil {
 			c.Close()
 			return nil, fmt.Errorf("sqlite: open %q: %w", path, err)
 		}
-		if _, err := stmt.Step(); err != nil {
-			stmt.Finalize()
+		_, err = stmt.Step()
+		stmt.Finalize()
+		if err != nil {
 			c.Close()
-			return nil, fmt.Errorf("setting wal journal mode: %w", err)
+			return nil, fmt.Errorf("sqlite: open %q: enable wal: %w", path, err)
 		}
-		defer stmt.Finalize()
 	}
 
+	c.SetBlockOnBusy()
 	return c, nil
 }
 
