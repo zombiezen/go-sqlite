7830b851045926963483e2d9ddaf8d27b513a0cb
Ross Light <ross@zombiezen.com>
2023-02-17 10:14:51 -0800

    Update dev shell to Go 1.20

--- shell.nix
+++ shell.nix
@@ -1,8 +1,8 @@
-{ pkgs ? import (fetchTarball "https://github.com/NixOS/nixpkgs/archive/0e857e0089d78dee29818dc92722a72f1dea506f.tar.gz") {}
+{ pkgs ? import (fetchTarball "https://github.com/NixOS/nixpkgs/archive/8c66bd1b68f4708c90dcc97c6f7052a5a7b33257.tar.gz") {}
 }:
 
 pkgs.mkShell {
   packages = [
-    pkgs.go_1_19
+    pkgs.go_1_20
   ];
 }
