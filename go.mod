11ea1eb14525f2fb980c35dd4287864dc5a3d154
Ross Light <ross@zombiezen.com>
2023-12-05 15:20:05 -0800

    Use flake for Nix dev shell
    Unify Go version on 1.20

--- go.mod
+++ go.mod
@@ -1,6 +1,6 @@
 module zombiezen.com/go/sqlite
 
-go 1.19
+go 1.20
 
 require (
 	crawshaw.io/iox v0.0.0-20181124134642-c51c3df30797
--- shell.nix
+++
@@ -1,8 +0,0 @@
-{ pkgs ? import (fetchTarball "https://github.com/NixOS/nixpkgs/archive/8c66bd1b68f4708c90dcc97c6f7052a5a7b33257.tar.gz") {}
-}:
-
-pkgs.mkShell {
-  packages = [
-    pkgs.go_1_20
-  ];
-}
